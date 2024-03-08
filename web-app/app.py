f3aab985f03fd19552714a1ff4fd5c327d84af84
RaiDeOliveira <rai.caje@sou.inteli.edu.br>
2024-03-08 16:07:47 -0300

    POST method working

--- web-app/app.py
+++ web-app/app.py
@@ -1,30 +1,35 @@
 from flask import Flask, render_template, request, redirect, url_for
 from tinydb import TinyDB
 
+# Instância do Flask
 app = Flask(__name__)
 
-db = TinyDB()
+# Instância do banco de dados
+db = TinyDB("web-app/caminhos.json")
 
+# Rota para salvar o caminho enviado pelo usuário na rota "/novo"
 @app.route("/pegar_caminho", methods=["GET", "POST"])
-def pegar_caminho(caminhoNovoID, caminhoNovoX, caminhoNovoY, caminhoNovoZ, caminhoNovoR):
+def pegar_caminho(caminhoNovoID=None, caminhoNovoX=None, caminhoNovoY=None, caminhoNovoZ=None, caminhoNovoR=None):
     if request.method == "POST":
         caminhoNovoID = str(request.form.get("caminhoNovoID"))
         caminhoNovoX = request.form.get("caminhoNovoX")
         caminhoNovoY = request.form.get("caminhoNovoY")
         caminhoNovoZ = request.form.get("caminhoNovoZ")
         caminhoNovoR = request.form.get("caminhoNovoR")
-        db.insert({caminhoNovoID: {caminhoNovoX, caminhoNovoY, caminhoNovoZ, caminhoNovoR}})
+        db.insert({"ID": caminhoNovoID, "X": caminhoNovoX, "Y": caminhoNovoY, "Z": caminhoNovoZ, "R": caminhoNovoR})
     caminhos = db.all()
-    return listas_caminhos(ID=caminhoNovoID, caminhos=caminhos)
+    return listas_caminhos()
 
+# Rota para enviar um novo caminho
 @app.route("/novo")
 def novo():
     return render_template("novo.html")
 
+# Rota para exibir lista de caminhos
 @app.route("/listas_caminhos")
-def listas_caminhos(ID, caminhos):
-    render_template("listaCaminhos.html", ID, caminhos)
+def listas_caminhos():
+    render_template("listaCaminhos.html")
 
-
+# Roda a aplicação web localmente quando este arquivo é executado
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
